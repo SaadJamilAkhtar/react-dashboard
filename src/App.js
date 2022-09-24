c8393420ab3a928ed9f9a8e10d7de7f5875314d4
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-09-25 04:23:49 +0500

    Added SideBar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {FiSettings} from "react-icons/fi";
 import ReactTooltip from 'react-tooltip';
 
 function App() {
+    const activeMenu = false;
     return (
         <div>
             <BrowserRouter>
@@ -16,14 +17,28 @@ function App() {
                             data-for='Settings-button'
                             type="button"
                             className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                            style={{background:'blue', borderRadius:'50%'}}
+                            style={{background: 'blue', borderRadius: '50%'}}
                         >
                             <FiSettings/>
                         </button>
                         <ReactTooltip place="top" id="Settings-button" type="dark" effect="solid">
                             <span>Settings</span>
                         </ReactTooltip>
-
+                    </div>
+                    {
+                        activeMenu
+                            ? <div className='w-72 fixed sidebar
+                        dark:bg-secondary-dark-bg bg-white'>
+                                SideBar
+                            </div>
+                            : <div className='w-0 dark:bg-secondary-dark-bg'>
+                                SideBar
+                            </div>
+                    }
+                    <div
+                        className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}
+                    >
+                        <div className="fixed"></div>
                     </div>
                 </div>
             </BrowserRouter>
